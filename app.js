48041a4e043407b6287d517fbcd37bdad7d720ff
JT Parker <jparkerdev@gmail.com>
2018-08-14 14:59:20 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var model = {
+  cats: [
+    {
+      name : 'Fluffy',
+      image : 'images/cat.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Furry',
+      image : 'images/cat2.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Hairy',
+      image : 'images/cat3.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Baldy',
+      image : 'images/cat4.jpg',
+      clickCount : 0
+    },
+    {
+      name : 'Mittens',
+      image : 'images/cat5.jpg',
+      clickCount : 0
+    }
+  ]
+};
+
+var octopus = {
+
+};
+
+var view = {
+
+};
