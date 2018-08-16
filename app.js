5deb1cb7f795320490fd25b89ca690a68936dbec
JT Parker <jparkerdev@gmail.com>
2018-08-15 22:16:24 -0600

    cat click not working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var model = {
+  selectedCat : null,
+
   cats: [
     {
       name : 'Fluffy',
@@ -12,12 +14,12 @@ var model = {
     },
     {
       name : 'Hairy',
-      image : 'images/cat3.jpg',
+      image : 'images/cat3.jpeg',
       clickCount : 0
     },
     {
       name : 'Baldy',
-      image : 'images/cat4.jpg',
+      image : 'images/cat4.jpeg',
       clickCount : 0
     },
     {
@@ -30,8 +32,84 @@ var model = {
 
 var octopus = {
 
+  init : function() {
+    model.selectedCat = model.cats[0];
+
+    catView.init();
+    catListView.init();
+
+  },
+
+  getCats : function() {
+    return model.cats;
+  },
+
+  getCurrentCat : function() {
+    return model.selectedCat;
+  },
+
+  setCurrentCat : function() {
+    model.selectedCat = cat;
+  },
+
+  incrementCat : function() {
+    model.selectedCat.clickCount++;
+    catView.render();
+  }
+
 };
 
-var view = {
+var catView = {
+
+  init : function() {
+    this.nameElem = document.getElementById('cat-name');
+    this.clickElem = document.getElementById('cat-clicks');
+    this.imgElem = document.getElementById('cat-img');
+
+    this.imgElem.addEventListener('click', function() {
+      octopus.incrementCat();
+    });
+
+    this.render();
+  },
+
+  render : function() {
+    var currentCat = octopus.getCurrentCat();
+    this.nameElem.innerHTML = currentCat.name;
+    this.clickElem.innerHTML = currentCat.clicks;
+    this.imgElem.src = currentCat.image;
+  }
 
 };
+
+var catListView = {
+
+  init : function() {
+    this.catListElem = document.getElementById('cat-list');
+
+    this.render();
+  },
+
+  render : function() {
+    var cats = octopus.getCats();
+
+    this.catListElem.innnerHTML = '';
+
+    for(var i = 0; i < cats.length; i++) {
+      var cat = cats[i];
+      var btn = document.createElement('button');
+      btn.textContent = cat.name;
+
+      btn.addEventListener('click', (function(catCopy) {
+          return function(catCopy) {
+            octopus.setCurrentCat(catCopy);
+            catView.render();
+          };
+      })(cat));
+
+      this.catListElem.appendChild(btn);
+    }
+  }
+};
+
+octopus.init();
